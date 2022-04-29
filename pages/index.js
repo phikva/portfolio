fe5f4bec814589bcbda5ef01e619a46b7f4a7b93
Philip Kvam <ph_kvam@hotmail.com>
2022-04-29 20:13:14 +0200

    tailwindfix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-7xl font-extrabold text-red-300">
       Hello world!
     </h1>
   )
